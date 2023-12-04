bbfff2da15c74583aa8924126d15bfa36b4feee9
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-04 17:52:53 -0500

    FIX Rubocop offenses

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
   def random_greeting
-    random_message = Message.order("RANDOM()").first
-    render json: { greeting: random_message&.message || "No greetings available" }
+    random_message = Message.order('RANDOM()').first
+    render json: { greeting: random_message&.message || 'No greetings available' }
   end
 end
