0c253b445acfb67078ca5e183a1ddd806edc5b05
Sergio Usma <sergioandresusma@hotmail.com>
2023-12-04 18:42:27 -0500

    Render React DOM in root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
+  root 'root#index'
+
+  namespace :api, defaults: { format: :json } do
     get 'greetings/random', to: 'api#random_greeting'
   end
 end
